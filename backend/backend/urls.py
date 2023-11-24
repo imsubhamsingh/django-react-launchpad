20139509c93ab652c7f2d40ec28c487638d758d5
Subham Singh <geekysubham@gmail.com>
2023-11-25 03:23:15 +0530

    run ruff linter to test repo

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 from myapp import views as myapp_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", myapp_views.index),
 ]
